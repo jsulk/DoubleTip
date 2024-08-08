ed36da942772e4251cf4a79101161ed4cb3f79cb
Jake Sulkoske <jakesulkoske@Jakes-MacBook-Pro-2.local>
2024-08-08 08:09:24 -0700

    Double Tip project initialized with Hello World

---
+++ DoubleTip/DoubleTipApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoubleTipApp.swift
+//  DoubleTip
+//
+//  Created by Jake Sulkoske on 8/8/24.
+//
+
+import SwiftUI
+
+@main
+struct DoubleTipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
