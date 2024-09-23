32cbe058e0b147ceaed461bcc41d1a82d53d226a
Jake Sulkoske <jakesulkoske@Jakes-MBP-2.lan>
2024-09-23 11:48:21 -0400

    added UI test setup files

--- Tests/DoubleTipUITests/DoubleTipUITests.swift
+++ Tests/DoubleTipUITests/DoubleTipUITests.swift
@@ -8,26 +8,18 @@
 import XCTest
 
 final class DoubleTipUITests: XCTestCase {
+    
+    private var app: XCUIApplication!
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+        app = XCUIApplication()
+        app.launch()
     }
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
-        app.launch()
-
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        super.tearDown()
+        app = nil
     }
 
     func testLaunchPerformance() throws {
@@ -37,5 +29,9 @@ final class DoubleTipUITests: XCTestCase {
                 XCUIApplication().launch()
             }
         }
+    }
+    
+    func test_calculate_button_action() {
+        
     }
 }
