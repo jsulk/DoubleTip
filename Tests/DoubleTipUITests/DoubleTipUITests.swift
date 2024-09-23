5c881ccc2e91b2ef7ce5e905cbb467ab8de8d161
Jake Sulkoske <jakesulkoske@Jakes-MBP-2.lan>
2024-09-23 13:32:05 -0400

    Added more tests for UI states

--- Tests/DoubleTipUITests/DoubleTipUITests.swift
+++ Tests/DoubleTipUITests/DoubleTipUITests.swift
@@ -45,5 +45,43 @@ final class DoubleTipUITests: XCTestCase {
         XCTAssertFalse(collectionViewsQuery/*@START_MENU_TOKEN@*/.staticTexts["TOTAL"]/*[[".cells.staticTexts[\"TOTAL\"]",".staticTexts[\"TOTAL\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.exists, "Total section title is showing on launch")
         XCTAssertFalse(collectionViewsQuery/*@START_MENU_TOKEN@*/.staticTexts["$120.00"]/*[[".cells.staticTexts[\"$120.00\"]",".staticTexts[\"$120.00\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.exists, "Total text field is showing on launch")
     }
+    
+    func testCalculateButtonActionHappyPath() {
+        let collectionViewsQuery = app.collectionViews
+        
+        collectionViewsQuery/*@START_MENU_TOKEN@*/.textFields["$0.00"]/*[[".cells.textFields[\"$0.00\"]",".textFields[\"$0.00\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        collectionViewsQuery/*@START_MENU_TOKEN@*/.textFields["$0.00"]/*[[".cells.textFields[\"$0.00\"]",".textFields[\"$0.00\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.typeText("$100.00")
+        
+        collectionViewsQuery/*@START_MENU_TOKEN@*/.textFields["15%"]/*[[".cells.textFields[\"15%\"]",".textFields[\"15%\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        collectionViewsQuery/*@START_MENU_TOKEN@*/.textFields["15%"]/*[[".cells.textFields[\"15%\"]",".textFields[\"15%\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.typeText("20%")
+        
+        dismissKeyboardIfPresent()
+        
+        collectionViewsQuery/*@START_MENU_TOKEN@*/.buttons["Calculate"]/*[[".cells.buttons[\"Calculate\"]",".buttons[\"Calculate\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        
+        XCTAssert(collectionViewsQuery/*@START_MENU_TOKEN@*/.staticTexts["TOTAL"]/*[[".cells.staticTexts[\"TOTAL\"]",".staticTexts[\"TOTAL\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.exists, "Total section title not showing when case satisfies")
+        XCTAssert(collectionViewsQuery/*@START_MENU_TOKEN@*/.staticTexts["$120.00"]/*[[".cells.staticTexts[\"$120.00\"]",".staticTexts[\"$120.00\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.exists, "Total text field not showing when case satisfies")
+    }
+    
+    func testCalculateButtonActionEmptyTipPercentage() {
+        let collectionViewsQuery = app.collectionViews
+        
+        collectionViewsQuery/*@START_MENU_TOKEN@*/.textFields["$0.00"]/*[[".cells.textFields[\"$0.00\"]",".textFields[\"$0.00\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        collectionViewsQuery/*@START_MENU_TOKEN@*/.textFields["$0.00"]/*[[".cells.textFields[\"$0.00\"]",".textFields[\"$0.00\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.typeText("$100.00")
+        
+        dismissKeyboardIfPresent()
+        
+        collectionViewsQuery/*@START_MENU_TOKEN@*/.buttons["Calculate"]/*[[".cells.buttons[\"Calculate\"]",".buttons[\"Calculate\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        
+        XCTAssertFalse(collectionViewsQuery/*@START_MENU_TOKEN@*/.staticTexts["TOTAL"]/*[[".cells.staticTexts[\"TOTAL\"]",".staticTexts[\"TOTAL\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.exists, "Total section title showing when tip percent empty")
+        XCTAssertFalse(collectionViewsQuery/*@START_MENU_TOKEN@*/.staticTexts["$120.00"]/*[[".cells.staticTexts[\"$120.00\"]",".staticTexts[\"$120.00\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.exists, "Total text field showing when tip percent empty")
+    }
 }
 
+extension DoubleTipUITests {
+    private func dismissKeyboardIfPresent() {
+        if app.keyboards.element(boundBy: 0).exists {
+            app.keyboards.buttons["return"].tap()
+        }
+    }
+}
